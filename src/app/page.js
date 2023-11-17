5da9e401bcbd12e819d205618e2d7db29ccf8542
Jose Soto <jsoto7087@gmail.com>
2023-11-16 23:21:10 -0500

    adds hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-
+import React from "react"
+import Hero from "./components/Hero"
 
 export default function Home() {
   return (
     <div>
-      Hello from Buldier Mobile Phebatony
+      <Hero />
     </div>
    
   )
