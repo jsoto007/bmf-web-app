01eb46ac40c164e0c010e56131ec81ff822711b6
Jose Soto <jsoto7087@gmail.com>
2023-11-25 22:31:50 -0500

    working with no errors

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import Image from 'next/image'
-
+import bmfLogo from "../../../public/bmfLogo.png"
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
@@ -16,17 +16,21 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white ">
+    <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Burdier Mobile Phlebotomy</span>
-              <img
+              <Image 
+                className='h-20 w-auto'
+                src={bmfLogo}
+              />
+              {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
-              />
+              /> */}
             </a>
           </div>
           <div className="flex lg:hidden">
