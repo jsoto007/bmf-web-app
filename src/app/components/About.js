16224c7afc4445a2506a7b36d2f2ba2d31666169
Jose Soto <jsoto7087@gmail.com>
2023-12-08 23:26:25 -0500

    remove comment

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -48,7 +48,6 @@ export default function About() {
             </div>
           </div>
         </div>
-        {/* This is the photo in the middle of the page */}
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <div className='bg-sky-700 w-[48rem]  sm:w-[57rem] max-w-none rounded-xl'>
             <Image
