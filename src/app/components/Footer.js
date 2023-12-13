6eeb419affb686ca15c24d5ef1a70c5ee1bf9803
Jose Soto <jsoto7087@gmail.com>
2023-12-12 23:51:46 -0500

    Handles Image loading

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
   
@@ -17,7 +18,7 @@ export default function Footer() {
 
   return (
     <div className="relative isolate overflow-hidden bg-neutral-100 py-24 sm:py-32">
-      {/* <img
+      {/* <Image
         src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
