d1d631fbba17dfb28551a7d6153b6d644c3478fe
Jose Soto <jsoto7087@gmail.com>
2023-12-14 22:19:28 -0500

    .

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -18,11 +18,7 @@ export default function Footer() {
 
   return (
     <div className="relative isolate overflow-hidden bg-neutral-100 py-24 sm:py-32">
-      {/* <Image
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      /> */}
+   {/* add back drop Image here */}
       <div
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
         aria-hidden="true"
